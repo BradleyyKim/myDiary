017f028790b5d4a661d58e34b6538cedba5f9721
BradleyyKim <win930323@gmail.com>
2022-08-17 21:51:44 +0900

    input, textarea focus 기능 추가

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styled from "styled-components";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const textareaContent = useRef();
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -17,30 +19,34 @@ const DiaryEditor = () => {
     });
   };
   const handleSubmit = () => {
-    console.log(state);
-    alert("saved!");
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      authorInput.current.placeholder = "1글자 이상 입력해 주세요 :)";
+      return;
+    }
+    if (state.content.length < 5) {
+      textareaContent.current.focus();
+      textareaContent.current.placeholder = "5글자 이상 입력해 주세요 :)";
+      return;
+    }
+    alert("저장되었습니다!");
   };
   return (
     <Container className="DiaryEditor">
       <Title>오늘의 일기</Title>
-      <div>
-        <input name="author" value={state.author} onChange={handleChangeState} />
-      </div>
-      <div>
-        <textarea name="content" value={state.content} onChange={handleChangeState}></textarea>
-      </div>
-      <div>
-        <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+      <Input ref={authorInput} name="author" value={state.author} onChange={handleChangeState} />
+      <Textarea ref={textareaContent} name="content" value={state.content} onChange={handleChangeState}></Textarea>
+      <SelectDiv>
+        오늘의 감정 점수 :
+        <Select name="emotion" value={state.emotion} onChange={handleChangeState}>
           <option value={1}>1</option>
           <option value={2}>2</option>
           <option value={3}>3</option>
           <option value={4}>4</option>
           <option value={5}>5</option>
-        </select>
-      </div>
-      <div>
-        <button onClick={handleSubmit}>일기 저장하기</button>
-      </div>
+        </Select>
+      </SelectDiv>
+      <Button onClick={handleSubmit}>일기 저장하기</Button>
     </Container>
   );
 };
@@ -48,13 +54,78 @@ const DiaryEditor = () => {
 const Container = styled.div`
   border: 1px solid #333;
   margin: 10px;
-  padding: 10px;
+  padding: 20px;
+  text-align: center;
+  border-radius: 20px;
+  border: 5px solid #0984e3;
 `;
 
 const Title = styled.h2`
   margin: 0 auto;
-  text-align: center;
   padding-bottom: 20px;
+  color: #0984e3;
 `;
 
+const Input = styled.input`
+  margin-bottom: 20px;
+  width: 500px;
+  padding: 10px;
+  border: 2px solid #0984e3;
+  border-radius: 10px;
+  ::placeholder {
+    color: ${(props) => (props.toggle ? "#a9a9a9" : "red")};
+  }
+
+  @media (max-width: 580px) {
+    width: 80%;
+  }
+`;
+
+const Textarea = styled.textarea`
+  margin-bottom: 20px;
+  width: 500px;
+  padding: 10px;
+  height: 150px;
+  border: 2px solid #0984e3;
+  border-radius: 10px;
+  ::placeholder {
+    color: ${(props) => (props.toggle ? "#a9a9a9" : "red")};
+  }
+  @media (max-width: 580px) {
+    width: 80%;
+  }
+`;
+const SelectDiv = styled.div`
+  color: #0984e3;
+`;
+const Select = styled.select`
+  width: 300px;
+  padding: 10px;
+  margin-bottom: 20px;
+  margin-left: 20px;
+  border: 2px solid #0984e3;
+  border-radius: 10px;
+  @media (max-width: 580px) {
+    width: 50%;
+  }
+`;
+
+const Button = styled.button`
+  width: 500px;
+  padding: 10px;
+  cursor: pointer;
+  border: 2px solid #0984e3;
+  border-radius: 10px;
+  background-color: transparent;
+  color: #0984e3;
+  :hover {
+    border: 2px solid #fd79a8;
+    border-radius: 10px;
+    background-color: transparent;
+    color: #fd79a8;
+  }
+  @media (max-width: 580px) {
+    width: 50%;
+  }
+`;
 export default DiaryEditor;
